1adc20c88b50a392dff4eae0e3628ba5d02a9ebe
Robert Cardona <rfcardona@gmail.com>
2015-08-20 14:02:22 -0400

    fixed profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_this_user, :except => [:show, :index]
+  before_action :authenticate_this_user, :except => [:new, :show, :index]
 
   # GET /profiles
   # GET /profiles.json
