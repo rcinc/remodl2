5a33b1f241d54c61e13427e3b9fd0ef6610d1152
Ravi Bakhai <ravi.bakhai@gmail.com>
2015-08-21 13:38:00 -0400

    added upvote mehtod

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -66,6 +66,11 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def upvote
+    @profile = Profile.find(params[:id])
+    @profile.votes.create
+    redirect_to(profiles_path)
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
