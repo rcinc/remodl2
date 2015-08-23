7256b9ad5158371589bd4b6a013bb55b27c464d4
Robert Cardona <rfcardona@gmail.com>
2015-08-23 10:50:08 -0400

    added correct view for profile page depending on user or vendor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :name
 
   end
+
+  def after_sign_in_path_for(resource)
+    new_profile_path
+  end
+
 end
