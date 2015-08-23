927d9630ef460547b5be52aafa7bef1f0bce618a
Robert Cardona <rfcardona@gmail.com>
2015-08-23 13:06:33 -0400

    modified dashboard

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -75,6 +75,6 @@ class BidsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def bid_params
-      params.require(:bid).permit(:bid)
+      params.require(:bid).permit(:bid, :accepted)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,9 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
+  get 'projects/:id/closed_project' => 'projects#closed_project', :as => 'closed_project'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
