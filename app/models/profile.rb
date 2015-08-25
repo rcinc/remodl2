1c60ebd553e0a0fbd7da1a3f1b0d89c6a5650b46
Robert Cardona <rfcardona@gmail.com>
2015-08-24 22:55:42 -0400

    attr

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   has_many :downvotes, dependent: :destroy
   belongs_to :user
+  attr_accessible :attachment
   has_attached_file :attachment, styles: {
   thumb: '100x100>',
   square: '200x200#',
